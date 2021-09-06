0dbae9032622b2fef89966bbaa553575c6700781
trungnguyen803 <90129592+trungnguyen803@users.noreply.github.com>
2021-09-06 15:24:55 +0700

    Add table brand and category

---
+++ database/migrations/2021_09_06_041609_create_category_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('category_name');
+            $table->text('category_slug');
+            $table->Integer('category_parent');
+            $table->Integer('brand_id');
+            $table->text('meta_keywords');
+            $table->text('meta_title');
+            $table->text('meta_desc');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category');
+    }
+}
---
+++ database/migrations/2021_09_06_041706_create_brand_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBrandTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brand', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('brand_name');
+            $table->text('brand_slug');
+            $table->Integer('category_id');
+            $table->text('meta_keywords');
+            $table->text('meta_title');
+            $table->text('meta_desc');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brand');
+    }
+}
